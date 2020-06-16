0fbd62393c65899669df95da0bf55a7408c1b26f
Liwen Qiao <lianna.qiao.lw@gmail.com>
2020-06-16 12:48:31 -0400

    Initial Commit

---
+++ OOTD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOTD
+//
+//  Created by qiaoliwen on 2020/6/16.
+//  Copyright © 2020 qiaoliwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
